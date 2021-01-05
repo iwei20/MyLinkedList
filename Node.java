cec0f977b82918af5727bd8c8d1f4fcaf3a5bfaa
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-01-05 13:19:59 -0500

    Write getters and setters

--- Node.java
+++ Node.java
@@ -2,4 +2,27 @@ public class Node{
     private String data;
     private Node next,prev;
     //write get/set methods for all three instance variables.
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
 }
